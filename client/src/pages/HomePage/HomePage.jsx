843e3d7baa661319d84df82bb96cce3d3297a9f4
einatshahal <einatshahal@gmail.com>
2025-01-13 11:25:34 +0200

    display opening screen

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -13,23 +13,25 @@ const HomePage = () => {
 
   return (
     <div className={styles.home}>
-           
+
       <img src={bulling} alt="Bulling illustration" className={styles.image} />
+
       <div className={styles["text-container"]}>
 
-      <h1 className={styles.headline}>מתמודדים עם פגיעות ברשת</h1>
-      <p className={styles.subheading}>זיהוי פגיעות. מתן הכוונה. תמיכה אישית.</p>
+        <h1 className={styles.headline}>מתמודדים עם פגיעות ברשת</h1>
+        <p className={styles.subheading}>זיהוי פגיעות. מתן הכוונה. תמיכה אישית.</p>
         
-      <p className={styles.description}>אם נתקלתם בתוכן ברשת שגרם לכם להרגיש לא בנוח,
+        <p className={styles.description}>אם נתקלתם בתוכן ברשת שגרם לכם להרגיש לא בנוח,
        חוויתם חוויה לא נעימה,
         או שאתם מעוניינים לדווח ולבקש עזרה,
         התחילו בשיחה ותקבלו הכוונה למענה המתאים ביותר עבורכם.
         </p>
-        </div>
 
         <StartQ className={styles['start-button']} onClick={handleStartClick} />
-
         </div>
+
+
+    </div>
 
   );
 };
